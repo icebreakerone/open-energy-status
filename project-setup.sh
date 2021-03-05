a598e2296edafc25270f2ffd9b049977ac714f00
Kasia <katarzyna.kulma@gmail.com>
2021-03-05 13:21:40 +0000

    add project-setup.sh

---
+++ project-setup.sh
@@ -0,0 +1,12 @@
+## instal poetry
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+
+## initialize the project
+# cd my_project
+poetry init -n
+
+## instal dependencies 
+poetry add --dev pytest pytest-cov
+poetry add --dev black --allow-prereleases
+poetry add --dev requests
+# poetry add --dev re sys # fails on my local machine
